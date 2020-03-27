214549964e422a4eab56ee62437a8f65c692ced7
Kennedy <kennedybacelar@yahoo.com.br>
2020-03-27 19:18:48 +0100

    After connecting to MongoDB. Methods Patch and deleted added.

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
  
+mongoose.connect(
+    'mongodb+srv://kennedy:' + 
+    process.env.MONGO_ATLAS_PW +
+    '@cluster0-35wpj.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true, useUnifiedTopology: true
+    }
+);      
+
 app.use(morgan('dev')); //midleware to treat routes
 //The below commands provides info about the request
 app.use(bodyParser.urlencoded({extended: false})); //another midleware to treat requests
